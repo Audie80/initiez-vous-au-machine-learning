e7e403c6812f88e6f55a655e0d88ac11b116fb67
Audie80 <audie.menut@gmail.com>
2026-09-08 20:30:48 +0200

    Initialisation et écriture du premier script

---
+++ README.txt
@@ -0,0 +1,18 @@
+Repository de mise en application du cours "Initiez-vous au Machine Learning" sur Open Classrooms.
+https://openclassrooms.com/fr/courses/8063076-initiez-vous-au-machine-learning
+
+Pour recréer l'environnemnt :
+```
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+```
+Pour exécuter le code :
+```
+cd ~/initiez-vous-au-machine-learning
+source .venv/bin/activate
+python entrainement-model.py
+python prediction-poids.py
+deactivate
+Quit()
+```
---
+++ prediction-poids.py
@@ -0,0 +1,25 @@
+import pandas as pd
+df = pd.read_csv('data/age_vs_taille_vs_poids_vs_sexe.csv')
+
+# les variables prédictives
+X = df[['sexe','age', 'taille']]
+
+# la variable cible, le poids
+y = df.poids
+
+# on choisit un modèle de régression linéaire
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+
+# on entraîne ce modèle sur les données avec la méthode fit
+reg.fit(X, y)
+
+# Exemple de donnée à prédire
+import numpy as np
+nouvelle_donnee = pd.DataFrame(
+    [[0, 150, 153]],
+    columns=['sexe', 'age', 'taille']
+)
+
+prediction = reg.predict(nouvelle_donnee)
+print(prediction)
