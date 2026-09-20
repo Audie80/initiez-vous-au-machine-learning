93d20e9d09cd01658062db064c2c27e95f8d11a5
Audie80 <audie.menut@gmail.com>
2026-09-20 18:51:37 +0200

    Correction du linter et des tests

--- linear-regression-1.py
+++ linear-regression-1.py
@@ -1,15 +1,16 @@
 # entraînement du modèle de régression linéaire sur le dataset age_vs_taille_vs_poids_vs_sexe.csv
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 df = pd.read_csv('data/age_vs_taille_vs_poids_vs_sexe.csv')
 
 # les variables prédictives
-X = df[['sexe','age', 'taille']]
+X = df[['sexe', 'age', 'taille']]
 
 # la variable cible, le poids
 y = df.poids
 
 # on choisit un modèle de régression linéaire
-from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
 
 # on entraîne ce modèle sur les données avec la méthode fit
--- linear-regression-2.py
+++ linear-regression-2.py
@@ -1,22 +1,22 @@
 # prédiction du poids à partir de l'âge, de la taille et du sexe avec un modèle de régression linéaire
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 df = pd.read_csv('data/age_vs_taille_vs_poids_vs_sexe.csv')
 
 # les variables prédictives
-X = df[['sexe','age', 'taille']]
+X = df[['sexe', 'age', 'taille']]
 
 # la variable cible, le poids
 y = df.poids
 
 # on choisit un modèle de régression linéaire
-from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
 
 # on entraîne ce modèle sur les données avec la méthode fit
 reg.fit(X, y)
 
 # Exemple de donnée à prédire
-import numpy as np
 nouvelle_donnee = pd.DataFrame(
     [[0, 150, 153]],
     columns=['sexe', 'age', 'taille']
